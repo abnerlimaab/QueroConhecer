4ce7f31c350b81dacaeb22d780fa12680c7d1c35
Abner Lima <abnerlima.1997@gmail.com>
2023-03-25 14:10:39 -0300

    CLGeocoder - Pesquisando cidades e locais diversos

--- QueroConhecer/PlaceFinderViewController.swift
+++ QueroConhecer/PlaceFinderViewController.swift
@@ -20,7 +20,25 @@ class PlaceFinderViewController: UIViewController {
     }
     
     @IBAction func findCity(_ sender: UIButton) {
+        tfCity.resignFirstResponder()
+        let address = tfCity.text!
+        load(show: true)
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            self.load(show: false)
+            
+            guard let placemark = placemarks?.first else {return}
+            
+            print(Place.getFormattedAddress(with: placemark))
+        }
     }
+    
+    func load(show: Bool) {
+        viLoading.isHidden = !show
+        
+        show ? aiLoading.startAnimating() : aiLoading.stopAnimating()
+     }
     
     @IBAction func close(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
