3e9a3482de96ef3f84b0ec380adf252370fd96b7
Abner Lima <abnerlima.1997@gmail.com>
2023-03-25 06:49:45 -0300

    Initial Commit

---
+++ QueroConhecer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QueroConhecer
+//
+//  Created by Abner Lima on 25/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
