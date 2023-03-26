a52545fae1da9066a25a0d761f428970107e7f9b
Abner Lima <abnerlima.1997@gmail.com>
2023-03-26 12:17:09 -0300

    Definindo regiões no mapa e usando alertas

--- QueroConhecer/PlaceFinderViewController.swift
+++ QueroConhecer/PlaceFinderViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 import MapKit
 
 class PlaceFinderViewController: UIViewController {
+    
+    enum PlaceFinderMessageType {
+        case error(String)
+        case confirmation(String)
+    }
         
     @IBOutlet weak var tfCity: UITextField!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var aiLoading: UIActivityIndicatorView!
     @IBOutlet weak var viLoading: UIView!
+    
+    var place: Place!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +35,61 @@ class PlaceFinderViewController: UIViewController {
         geoCoder.geocodeAddressString(address) { (placemarks, error) in
             self.load(show: false)
             
-            guard let placemark = placemarks?.first else {return}
+            if error == nil {
+                if !self.savePlace(with: placemarks?.first) {
+                    self.showMessage(type: .error("Não foi encontrado nenhum local com esse nome."))
+                }
+            } else {
+                self.showMessage(type: .error("Erro desconhecido."))
+            }
             
-            print(Place.getFormattedAddress(with: placemark))
         }
+    }
+    
+    func savePlace(with placemark: CLPlacemark?) -> Bool {
+        guard let placemark = placemark, let coordinate = placemark.location?.coordinate else {
+            return false
+        }
+        
+        let name = placemark.name ?? placemark.country ?? "Desconhecido"
+        let address = Place.getFormattedAddress(with: placemark)
+        place = Place(name: name, latitude: coordinate.latitude, longitude: coordinate.longitude, address: address)
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 2500, longitudinalMeters: 2500)
+        
+        mapView.setRegion(region, animated: true)
+        
+        self.showMessage(type: .confirmation(place.name))
+        
+        return true
+    }
+    
+    func showMessage(type: PlaceFinderMessageType) {
+        let title: String, message: String
+        var hasConfirmation: Bool = false
+        
+        switch type {
+        case .confirmation(let name):
+            title = "Local encontrado"
+            message = "Deseja adicionar \(name)?"
+            hasConfirmation = true
+        case .error(let errorMessage):
+            title = "Erro"
+            message = errorMessage
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel)
+        alert.addAction(cancelAction)
+        
+        if hasConfirmation {
+            let confirmAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+                print("Ok")
+            }
+            alert.addAction(confirmAction)
+        }
+        
+        present(alert, animated: true)
     }
     
     func load(show: Bool) {
